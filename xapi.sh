70aee16637f706c7ed9cc8b7600598e509bd7de6
a80815213 <157588128+a80815213@users.noreply.github.com>
2025-02-19 11:56:36 +0800

    Update xapi.sh

--- xapi.sh
+++ xapi.sh
@@ -52,10 +52,9 @@ add_chaotic_aur() {
         sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
         sudo pacman-key --lsign-key 3056513887B78AEB
         sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'  
-        sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-        echo -e '\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a /etc/pacman.conf
-        touch /etc/pacman.d/chaotic-mirrorlist
-        echo
+        sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'  
+        echo -e '\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a /etc/pacman.conf    
+        echo  
         echo "Chaotic-AUR Repository added!"  
         echo
         sleep 3
